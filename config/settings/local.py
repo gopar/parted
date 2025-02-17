d8aac85142f4574a6d4930e93eb85208d64b902b
Gopar <gopardaniel@gmail.com>
2025-02-16 22:09:25 -0800

    Add rich library and configuration

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,1 +1,38 @@
 from .base import *
+
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "filters": {
+        "require_debug_false": {
+            "()": "django.utils.log.RequireDebugFalse",
+        },
+        "require_debug_true": {
+            "()": "django.utils.log.RequireDebugTrue",
+        },
+    },
+    "formatters": {
+        "rich": {"datefmt": "[%X]"},
+    },
+    "handlers": {
+        "console": {
+            "class": "rich.logging.RichHandler",
+            "filters": ["require_debug_true"],
+            "formatter": "rich",
+            "level": "DEBUG",
+            "rich_tracebacks": True,
+            "tracebacks_show_locals": True,
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": [],
+            "level": "INFO",
+        },
+        "root": {
+            "handlers": ["console"],
+            "level": "INFO",
+        },
+    },
+}
