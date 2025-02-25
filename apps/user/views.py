964fe2ba970dfb3b50661c764ef65429aeb015cf
Gopar <gopardaniel@gmail.com>
2025-02-24 17:22:43 -0800

    Basic views

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request: HttpRequest) -> HttpResponse:
+    return render(request, "templates/base/index.html")
+
+
+def about(request: HttpRequest) -> HttpResponse:
+    return render(request, "templates/base/about.html")
