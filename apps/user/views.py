7cae3193e81265878086abc0cead975a0c3fd408
Gopar <gopardaniel@gmail.com>
2025-03-03 10:55:05 -0800

    Refactor structure of html files

--- apps/user/views.py
+++ apps/user/views.py
@@ -3,8 +3,13 @@ from django.shortcuts import render
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    return render(request, "base/index.html")
+    mock_artist = {
+        "name": "John Doe",
+        "profile_image": "https://placehold.co/200x200",
+        "main_genre": "Jazz",
+    }
+    return render(request, "pages/home_feed.html", context={"artists": [mock_artist for _ in range(5)]})
 
 
 def about(request: HttpRequest) -> HttpResponse:
-    return render(request, "base/about.html")
+    return render(request, "pages/about.html")
