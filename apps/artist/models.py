b0eb5bcbeffb1f43eb282bd8298cfdbbf440893f
Gopar <gopardaniel@gmail.com>
2025-03-03 13:45:30 -0800

    Add genre model

--- apps/artist/models.py
+++ apps/artist/models.py
@@ -3,6 +3,16 @@ from django.db import models
 from model_utils.models import TimeStampedModel
 
 
+class Genre(TimeStampedModel):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+
+
 class Artist(TimeStampedModel):
     name = models.CharField(max_length=255, blank=False, null=False)
     members = models.ManyToManyField(
@@ -10,6 +20,14 @@ class Artist(TimeStampedModel):
         related_name="artist_profiles",
         blank=True,
     )
+    main_genre = models.ForeignKey(
+        Genre,
+        on_delete=models.SET_NULL,
+        related_name="primary_artists",
+        null=True,
+        blank=True,
+    )
+    genres = models.ManyToManyField(Genre, related_name="artists", blank=True)
 
     def __str__(self) -> str:
         return self.name
